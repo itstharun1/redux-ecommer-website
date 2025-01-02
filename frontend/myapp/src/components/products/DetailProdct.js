9e0ab1dade737ed8a0d83777a4e1179ec5e77bd1
itstharun1 <tharunganjai@gmail.com>
2025-01-02 10:34:34 +0530

    main

--- frontend/myapp/src/components/products/DetailProdct.js
+++ frontend/myapp/src/components/products/DetailProdct.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
+import Nav from '../dashboard/Nav'
 
 function DetailProdct() {
+  const [prdt,setPrdt]=useState()
+  const { id } = useParams()
+  const fetchProducts = async () => {
+    const response = await axios
+      .get(`https://fakestoreapi.com/products/${id}`)
+      .catch((err) => {
+        console.log("Err: ", err);
+      });
+      setPrdt(response.data)
+
+  };
+  useEffect(() => {
+      fetchProducts();
+    }, []);
+ 
+
   return (
     <div>
-      <h1>Coming soon</h1>
+      <Nav />
+      <div>
+          <img className='img-div' src={prdt.image} alt='img'/>
+          <p>{prdt.title}</p>
+          <p>{prdt.price}</p>                  
+      </div>
     </div>
   )
 }
--- frontend/myapp/src/index.js
+++ frontend/myapp/src/index.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import store from './components/redux/store'
-import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
@@ -9,8 +7,8 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
+
     <App />
-    </Provider>
+
   </React.StrictMode>
 );
